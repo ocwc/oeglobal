a3bd8a7cee87d582124b5d07855f27370fbe4d1e
Jure Cuhalev <jure@cuhalev.com>
2019-11-06 18:04:15 +0100

    Improves mobile pages

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,5 +1,5 @@
 <article @php(post_class('container'))>
-  <div class="content mb-6 p-6 w-2/3 ml-1/12">
+  <div class="content mb-6 p-6 w-full lg:w-2/3 lg:ml-1/12">
     @php(the_content())
   </div>
 </article>
