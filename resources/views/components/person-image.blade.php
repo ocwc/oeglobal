54d0e5d90b9f5dc9b69f4987a54118a135a707fc
Jure Cuhalev <jure@cuhalev.com>
2019-11-11 14:02:31 +0100

    Update person image background pattern

--- resources/views/components/person-image.blade.php
+++ resources/views/components/person-image.blade.php
@@ -1,5 +1,4 @@
-<div class="person-image inline-block relative">
-  <img class="rounded-full relative z-10" src="{!! $src !!}"
+<div class="person-image inline-block">
+  <img class="absolute rounded-full" src="{!! $src !!}"
        alt="{!! $alt !!}">
-  <img class="rounded-full person-image__texture" src="@asset('images/textures/person-texture.png')" alt="" aria-hidden>
 </div>
