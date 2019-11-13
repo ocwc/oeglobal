a455419c193f7472150d5b3b0c43d3991726b199
Jure Cuhalev <jure@cuhalev.com>
2019-11-13 19:17:30 +0100

    Make sublanding pages full-width

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,5 +1,7 @@
 <article @php(post_class('container'))>
-  <div class="content mb-6 p-6 w-full lg:w-2/3 lg:ml-1/12">
+  <div class="content mb-6 p-6 w-full
+    @if ($variant != 'sublanding') lg:w-2/3 lg:ml-1/12 @endif
+    ">
     @php(the_content())
   </div>
 </article>
