9c1a58570ebbc7f3913a72051c016d63a63d52f8
Jure Cuhalev <jure@cuhalev.com>
2020-01-11 15:46:04 +0700

    Tweak header color

--- resources/views/components/global-header.blade.php
+++ resources/views/components/global-header.blade.php
@@ -1,4 +1,4 @@
-<div class="global-header bg-main
+<div class="global-header bg-shade-40
             w-full hidden md:flex flex-row items-center align-center justify-end
             lg:h-6
             uppercase text-2xs lg:text-xs leading-loose font-sans text-right">
