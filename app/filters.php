21144eae632c6ed029e045ac4d82199da4a0e08b
Jure Cuhalev <jure@cuhalev.com>
2020-01-16 20:22:34 +0700

    Removes 'category' from archive pages

--- app/filters.php
+++ app/filters.php
@@ -136,3 +136,16 @@ add_filter( 'block_categories', function ( $categories, $post ) {
 add_filter('get_search_form', function () {
     return \App\template( 'partials.searchform' );
 });
+
+add_filter( 'get_the_archive_title', function ($title) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+    } elseif ( is_tax() ) { //for custom post types
+        $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+    }
+    return $title;
+});
