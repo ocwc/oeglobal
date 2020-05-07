f8af2309d4fdd2eb118c97acd53ef7bbd5559a24
marbaque <mariobadilla@gmail.com>
2020-05-06 19:24:21 -0600

    deleted old colors and fixed blue-90

--- resources/assets/styles/tailwind.config.js
+++ resources/assets/styles/tailwind.config.js
@@ -60,42 +60,10 @@ module.exports = {
         'fuchsia-700': '#e77ee3',
         'fuchsia-800': '#f0b2ec',
         'fuchsia-900': '#f7d4f2',
-        // main: '#0D59F2',
-        // 'main-dark': '#195BB3',
-        // 'main-darker': '#1F467A',
-        // 'main-light': '#3D7AF5',
-        // 'main-lighter': '#6E9CF7',
-        // 'main-lightest': '#9EBDFA',
-        // int: {
-        //   turq: '#26D7B2',
-        //   yellow: '#FFE525',
-        //   red: '#FF5547',
-        //   fuchsia: '#DB43D8',
-        // },
         latam: {
           orange: '#FF922D',
           'orange-cream': '#FFC794',
         },
-        // shade: {
-        //   10: '#0A1729',
-        //   20: '#142F52',
-        //   30: '#1F467A',
-        //   40: '#195BB3',
-        //   50: '#0D59F2',
-        //   60: '#3D7AF5',
-        //   70: '#6E9CF7',
-        //   80: '#9EBDFA',
-        //   90: '#CFDEFC',
-        // },
-        // black: '#333333',
-        // black2: '#4D4D4D',
-        // dark: '#4A4A4A',
-        // gray1: '#666666',
-        // gray2: '#999999',
-        // gray3: '#CCCCCC',
-        // gray4: '#E6E6E6',
-        // gray5: '#F2F2F2',
-        // light: '#E6E6E6',
       },
       lineHeight: {
         tight: 1.2,
@@ -160,5 +128,5 @@ if (SITE === 'latam') {
   module.exports.theme.extend.colors.menuHover = module.exports.theme.extend.colors['gray-900'];
 } else {
   module.exports.theme.extend.colors.primary = module.exports.theme.extend.colors.main;
-  module.exports.theme.extend.colors.menuHover = module.exports.theme.extend.colors['blue-90'];
+  module.exports.theme.extend.colors.menuHover = module.exports.theme.extend.colors['blue-900'];
 }
