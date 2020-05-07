9d40d1b1c93ec2b0bba46359d597fad656dc4c14
marbaque <mariobadilla@gmail.com>
2020-05-06 19:22:19 -0600

    changed colors in tailwind.config, sass, & views

--- resources/views/components/oeg-projects-collection.blade.php
+++ resources/views/components/oeg-projects-collection.blade.php
@@ -2,7 +2,7 @@
 <div class="flex flex-wrap -mx-2 overflow-hidden">
   @foreach ($items as $item)
     <div class="my-2 px-2 w-full lg:w-1/3 overflow-hidden">
-      <div class="bg-gray4 p-6">
+      <div class="bg-gray-900 p-6">
         <a href="{!! $item['url'] !!}" class="text-center pb-4 block">
           <img class="m-auto h-16 lg:h-12" src="{!! $item['image'] !!}" alt="{!! $item['name'] !!}"/>
         </a>
