8de5b34178f6b59eb92e4ba941f1d66cfae10218
marbaque <mariobadilla@gmail.com>
2020-06-17 22:43:37 -0600

    view all links

--- resources/views/components/oeg-featured.blade.php
+++ resources/views/components/oeg-featured.blade.php
@@ -1,22 +1,19 @@
 <div class="container px-0 md:px-4 mt-6 md:mt-12 mb-0">
-  <h2 class="text-black-200 border-b border-gray-400 text-xl font-bold font-sans pb-2 mb-6 pl-4 md:pl-0">Happening now!</h2>
+  <h2 class="text-black-200 border-b border-gray-400 text-xl font-bold font-sans pb-2 mb-6 mx-4 md:mx-0 md:pl-0">Happening now!</h2>
 
   <div class="grid grid-rows-auto grid-cols-auto md:grid-rows-2 md:grid-cols-3 gap-2 md:gap-4">
     @foreach ($featured_items as $item)
-      <a class="
+    <a class="
           oeg-featured {!! 'oeg-featured-' . $loop->iteration !!}
       @if($loop->iteration === 1)md:row-span-2 md:col-span-2
           @else col-auto row-auto
           @endif
         flex items-end
-"
-         style="background-image: url('{!! $item['image']['sizes']['large'] !!}');"
-         href="{!! $item['url'] !!}"
-      >
-        <span class="oeg-featured__title">
-          {!! $item['title'] !!}
-        </span>
-      </a>
+" style="background-image: url('{!! $item['image']['sizes']['large'] !!}');" href="{!! $item['url'] !!}">
+      <span class="oeg-featured__title">
+        {!! $item['title'] !!}
+      </span>
+    </a>
     @endforeach
 
   </div>
@@ -25,23 +22,21 @@
 <div class="oeg-spotlight relative overflow-hidden lg:overflow-visible pt-8 md:pt-20 lg:pt-20">
   <div class="container">
     <div class="oeg-spotlight__container relative">
-      <div class="flex w-full border-b border-gray-900 pb-2 mb-6 justify-between items-end">
-        <h2 class="flex text-gray-900 text-xl font-bold font-sans">Members spotlight</h2>
-        <a class="flex btn oeg-inverted" href="https://connect.oeglobal.org/c/oeg-plaza/spotlight/14">View more</a>
+      <div class="flex w-full border-b bg-blue-400 border-gray-900 pb-2 mb-6 justify-between items-end">
+        <h2 class="text-gray-900 text-xl font-bold font-sans">Members spotlight</h2>
+        <a class="text-gray-900" href="https://connect.oeglobal.org/c/oeg-plaza/spotlight/14">View more</a>
       </div>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         @foreach ($spotlight_items as $item)
-          <a class="oeg-spotlight__item" href="{!! $item['url'] !!}">
-          <span class="oeg-spotlight__image block"
-                style="background-image: url('{!! $item['image']['sizes']['medium'] !!}');"></span>
+        <a class="oeg-spotlight__item" href="{!! $item['url'] !!}">
+          <span class="oeg-spotlight__image block" style="background-image: url('{!! $item['image']['sizes']['medium'] !!}');"></span>
 
-            <span class="block px-4 py-6">
+          <span class="block px-4 py-6">
             <h2 class="h2 mb-4">{!! $item['title'] !!}</h2>
-            <span class="block mb-6">@svg('icons/check', 'h-auto inline-block')<span
-                class="ml-2">{!! $item['member'] !!}</span></span>
+            <span class="block mb-6">@svg('icons/check', 'h-auto inline-block')<span class="ml-2">{!! $item['member'] !!}</span></span>
             <span class="block font-serif">{!! $item['description'] !!}</span>
           </span>
-          </a>
+        </a>
         @endforeach
       </div>
     </div>
--- resources/views/partials/home-news.blade.php
+++ resources/views/partials/home-news.blade.php
@@ -1,15 +1,19 @@
 <div class="home-news container mt-10">
-  <h2 class="w-full border-b border-gray-400 text-xl font-sans pb-2 mb-2">News and updates</h2>
+  <div class="flex w-full border-b border-gray-400 pb-2 mb-6 justify-between items-end">
+    <h2 class="text-xl font-bold font-sans">News and Updates</h2>
+    <a class="flex" href="https://oeglobal.org/category/news/">View more</a>
+  </div>
   <div class="flex flex-wrap -mx-4">
     <div class="my-4 px-4 w-full lg:w-2/3">
       <div class="flex flex-wrap -mx-2">
         @foreach ($latest_news as $item)
-          @component('components.post-item', $item)@endcomponent
+        @component('components.post-item', $item)@endcomponent
         @endforeach
       </div>
     </div>
     <div class="my-4 px-4 w-full lg:w-1/3">
-      <a class="twitter-timeline" data-height="800" href="https://twitter.com/OpenEdGlobal?ref_src=twsrc%5Etfw">Tweets by OpenEdGlobal</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      <a class="twitter-timeline" data-height="800" href="https://twitter.com/OpenEdGlobal?ref_src=twsrc%5Etfw">Tweets by OpenEdGlobal</a>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
   </div>
 </div>
