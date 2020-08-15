8022a3340c4fb18a156ad368a4362459e57aee86
Jure Cuhalev <jure@cuhalev.com>
2020-08-15 19:05:40 +0200

    Rebuild how borders and box shadow works

--- resources/views/components/content-excerpt.blade.php
+++ resources/views/components/content-excerpt.blade.php
@@ -3,49 +3,52 @@
     @svg('icons/megaphone', 'h-22 w-22 cccoer__featured__icon')
   @endif
 
-  <a href="{!! $link !!}"
-     class="content content-excerpt
-              flex items-stretch justify-between flex-row overflow-hidden z-0 relative
-              shadow-box hover:shadow-xl rounded bg-white relative no-transition">
-
-
-    @if($image)
-      <span href="{!! $link !!}"
-            class="bg-cover bg-center w-1/4"
-            style="background-image: url('{!! $image !!}');">
+  <div
+    class="rounded border-2 overflow-hidden border-transparent
+           hover:border-turq-500 active:border-transparent
+           bg-white content-excerpt__shadow">
+    <a href="{!! $link !!}"
+       class="content content-excerpt
+              flex items-stretch justify-between flex-row z-10 relative
+               no-transition overflow-hidden">
+      @if($image)
+        <span href="{!! $link !!}"
+              class="bg-cover bg-center w-1/4"
+              style="background-image: url('{!! $image !!}');">
         </span>
-    @endif
+      @endif
 
-    <div class="
+      <div class="
       pl-8 px-6 pb-4 content-excerpt__body
         @if($image)w-3/4
         @else w-full
         @endif
-      ">
-      <header class="pt-6">
-        @if($terms)
-          <div class="">
-            @foreach ($terms as $term)
-              <span class="bg-none">{!! $term['name'] !!}</span>@if(!$loop->last), @endif
-            @endforeach
+        ">
+        <header class="pt-6">
+          @if($terms)
+            <div class="">
+              @foreach ($terms as $term)
+                <span class="bg-none">{!! $term['name'] !!}</span>@if(!$loop->last), @endif
+              @endforeach
+            </div>
+          @endif
+          <div class="mt-2 mb-4">
+            <h2 class="entry-title font-sans text-left pb-1 leading-snug text-xl hover-link inline">
+              {!! $title !!}
+            </h2>
+          </div>
+        </header>
+
+        <main class="entry-summary mt-2">
+          {!! $description !!}
+        </main>
+
+        @if($show_meta)
+          <div class="entry-meta">
+            @include('partials/entry-meta')
           </div>
         @endif
-        <div class="mt-2 mb-4">
-          <h2 class="entry-title font-sans text-left pb-1 leading-snug text-xl hover-link inline">
-            {!! $title !!}
-          </h2>
-        </div>
-      </header>
-
-      <main class="entry-summary mt-2">
-        {!! $description !!}
-      </main>
-
-      @if($show_meta)
-        <div class="entry-meta">
-          @include('partials/entry-meta')
-        </div>
-      @endif
-    </div>
-  </a>
+      </div>
+    </a>
+  </div>
 </article>
