6b664181be840569f2fb6ee2c5c245197a9a4c8f
Jure Cuhalev <jure@cuhalev.com>
2020-09-16 16:39:23 +0200

    Support award_category descriptions

--- resources/views/single-award.blade.php
+++ resources/views/single-award.blade.php
@@ -13,7 +13,15 @@
           @include('partials.content-single')
         </div>
         <div class="my-2 px-2 w-full lg:w-1/3">
-          @include('partials.sidebar')
+          @php $cat = wp_get_object_terms($post->ID, 'award_category'); @endphp
+          @if($cat)
+            <div class="bg-blue-700 p-6 rounded-lg">
+              <h2 class="font-bold text-xl mb-4">{!! $cat[0]->name !!}</h2>
+              <div class="italic">
+                {!! $cat[0]->description !!}
+              </div>
+            </div>
+          @endif
         </div>
       </div>
     </div>
