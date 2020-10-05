0b8e22ebace89d3920b1dd4d1ca7732a810b1d41
Jure Cuhalev <jure@cuhalev.com>
2020-10-05 18:05:18 +0200

    Update share icons

--- resources/views/components/content-share.blade.php
+++ resources/views/components/content-share.blade.php
@@ -2,10 +2,10 @@
   <div class="text-black-300 font-bold text-xl mb-4">Share this content</div>
   <div class="">
     <a href="https://twitter.com/intent/tweet?text={{ $title }} - {{ $site }} {{ $url }}"
-       target="_blank">@svg('icons/twitter', 'h-10 inline-block mr-2')</a>
+       target="_blank">@svg('icons/share-twitter', 'h-10 inline-block mr-2')</a>
     <a href="https://www.facebook.com/sharer/sharer.php?u={{ $url }}"
-       target="_blank">@svg('icons/facebook', 'h-10 inline-block mr-2')</a>
-    <a href="https://www.linkedin.com/sharing/share-offsite/?url={{ $url }}">@svg('icons/linkedin', 'h-10 inline-block mr-2')</a>
-    <a href="mailto:?subject={{ $title }} - {{ $site }}&body={{ $url }}">@svg('icons/mail', 'h-10 inline-block mr-2')</a>
+       target="_blank">@svg('icons/share-facebook', 'h-10 inline-block mr-2')</a>
+    <a href="https://www.linkedin.com/sharing/share-offsite/?url={{ $url }}">@svg('icons/share-linkedin', 'h-10 inline-block mr-2')</a>
+    <a href="mailto:?subject={{ $title }} - {{ $site }}&body={{ $url }}">@svg('icons/share-mail', 'h-10 inline-block mr-2')</a>
 </div>
 </div>
