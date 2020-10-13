92781b20cca5330b973d3be304a422923cda204e
Jure Cuhalev <jure@cuhalev.com>
2020-10-13 13:21:20 +0200

    Improve case study listing page

--- resources/views/archive-casestudy.blade.php
+++ resources/views/archive-casestudy.blade.php
@@ -16,12 +16,17 @@
     <div class="container">
       @php $first = true @endphp
       @while(have_posts()) @php the_post() @endphp
-      @if ($first && $site === 'oeglobal')
-        @php $first = false @endphp
-        @include('partials.content-full')
-      @else
-        @include('partials.content-excerpt')
-      @endif
+      @php($post = get_post())
+      @component('components/content-excerpt', [
+        'image' => get_the_post_thumbnail_url($post, 'large'),
+        'terms' => App::TermsWithLinks($post),
+        'link'  => get_permalink($post),
+        'title' => get_the_title($post),
+        'description' => \Illuminate\Support\Str::words( strip_tags(get_field( 'casestudy_overview', $post )), 64, ' [..]' ),
+        'show_meta' => true,
+        'featured' => false,
+        ])@endcomponent
+
       @endwhile
 
       <div class="flex justify-center">
