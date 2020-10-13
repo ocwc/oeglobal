15171df46137cd167b5805193c3c35a5dd119df5
Jure Cuhalev <jure@cuhalev.com>
2020-10-13 15:01:55 +0200

    Fix single view

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -31,7 +31,14 @@
             </div>
           </article>
 
-          @include('partials.content-single')
+          <div class="flex flex-col md:flex-row mt-10 justify-between items-stretch">
+            <div class="w-full prev-next-post prev md:mr-2 mb-4 md:mb-0">
+              @php(previous_post_link('%link', '<span>⇠ Previous</span><span class="title">%title</span>'))
+            </div>
+            <div class="w-full text-right prev-next-post next md:ml-2">
+              @php(next_post_link('%link', '<span>Next ⇢</span><span class="title">%title</a>'))
+            </div>
+          </div>
         </div>
         <div class="my-2 px-2 w-full lg:w-1/3">
           @include('partials.sidebar')
@@ -40,7 +47,7 @@
     </div>
     @endwhile
 
-    @if ($post_type === 'post' || $post_type === 'webinar')
+    @if (OEG_SITE === 'CCCOER' && $post_type === 'post' || $post_type === 'webinar')
       @component('components/section-header', ['title' => 'Related content', 'border' => 'border-gray-800'])@endcomponent
       <div class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach(App::RelatedPosts($post) as $post)
