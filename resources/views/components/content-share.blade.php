8bab9e1acd32609d78012b5797b830dc69c5179d
Jure Cuhalev <jure@cuhalev.com>
2020-10-17 12:46:36 +0200

    Tweak share

--- resources/views/components/content-share.blade.php
+++ resources/views/components/content-share.blade.php
@@ -1,5 +1,5 @@
 <div class="fill-current text-blue-500 flex flex-col items-center justify-center bg-gray-900 px-4 pt-6 pb-8 mb-10 rounded">
-  <div class="text-black-300 font-bold text-xl mb-4">Share this content</div>
+  <div class="text-black-300 font-bold text-xl mb-4">Share with your community</div>
   <div class="">
     <a href="https://twitter.com/intent/tweet?text={{ $title }} - {{ $site }} {{ $url }}"
        target="_blank">@svg('icons/share-twitter', 'h-10 inline-block mr-2')</a>
