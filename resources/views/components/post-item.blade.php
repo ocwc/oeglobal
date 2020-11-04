99c0f2a2858fc60ab7af2aba4c9f07e593353eef
marbaque <mariobadilla@gmail.com>
2020-11-04 03:37:11 -0600

    update mews post item to card design

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,23 +1,16 @@
 <div class="my-2 px-2 w-full lg:w-1/2">
-  <div class="post-item mb-6">
-    <div class="p-6">
-      <a href="{!! $url !!}">
+  <div class="post-item mb-6 rounded shadow-md">
+      <a class="block px-6 pt-6 pb-8" href="{!! $url !!}">
         <h2 class="h3">{!! $title !!}</h2>
+      
+        <p class="byline author vcard text-sm my-4 text-gray-500">
+          <span class="font-semibold font-sans">{{ __('By', 'sage') }} {!! $author !!}</span> {{ __('on', 'sage') }}
+            {!! $date !!}
+        </p>
+
+        <div class="text-sm font-normal leading-6 font-serif">
+          {!! $excerpt !!}
+        </div>
       </a>
-
-      <p class="byline author vcard text-sm my-2 text-gray-500">
-        <span class="font-semibold font-sans">{{ __('By', 'sage') }} {!! $author !!}</span> {{ __('on', 'sage') }}
-          {!! $date !!}
-      </p>
-
-
-      <div class="text-sm font-normal leading-relaxed">
-        {!! $excerpt !!}
-      </div>
-
-      <div class="mt-6">
-        <a href="{!! $url !!}">Learn more ...</a>
-      </div>
-    </div>
   </div>
 </div>
