f3559a57aa0d124810a3c7f23bf18396def716b7
marbaque <mario.badilla@gmail.com>
2021-05-19 22:31:56 -0600

    new elements nav bar, small fixes

--- app/setup.php
+++ app/setup.php
@@ -15,6 +15,9 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'sage/main.css', asset_path( 'styles/main.css' ), false, null );
     wp_enqueue_script( 'sage/main.js', asset_path( 'scripts/main.js' ), [ 'jquery' ], null, true );
 
+    wp_register_script( 'FontAwesome', 'https://kit.fontawesome.com/99fcecbc40.js', null, null, true );
+
+
     if ( is_single() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
     }
